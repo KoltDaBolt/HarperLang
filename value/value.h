359d15603ee54d93bd4edc193af1953cb596b627
Kolton Harper <koltdabolt1@gmail.com>
2024-07-20 21:21:43 -0500

    Support for dynamic types, type checking, comparison and equality operators

--- value/value.h
+++ value/value.h
@@ -3,7 +3,30 @@
 
 #include "../common.h"
 
-typedef double Value;
+typedef enum{
+    VAL_BOOL,
+    VAL_NULL,
+    VAL_NUMBER
+}ValueType;
+
+typedef struct{
+    ValueType type;
+    union{
+        bool boolean;
+        double number;
+    }as;
+}Value;
+
+#define BOOL_VAL(value)     ((Value){VAL_BOOL, {.boolean = value}})
+#define NULL_VALUE          ((Value){VAL_NULL, {.number = 0}})
+#define NUMBER_VAL(value)   ((Value){VAL_NUMBER, {.number = value}})
+
+#define AS_BOOL(value)      ((value).as.boolean)
+#define AS_NUMBER(value)    ((value).as.number)
+
+#define IS_BOOL(value)      ((value).type == VAL_BOOL)
+#define IS_NULL(value)      ((value).type == VAL_NULL)
+#define IS_NUMBER(value)    ((value).type == VAL_NUMBER)
 
 typedef struct{
     int count;
@@ -15,5 +38,6 @@ void initValueArray(ValueArray* array);
 void freeValueArray(ValueArray* array);
 void writeValueArray(ValueArray* array, Value value);
 void printValue(Value value);
+bool valuesEqual(Value a, Value b);
 
 #endif
